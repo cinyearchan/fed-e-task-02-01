38651c11bdf780b59b874840a19c34a2ed9f78a1
cinyearchan <cinyearchan@gmail.com>
2020-06-03 17:08:08 +0800

    finish grunt config

--- code/grunt/gruntfile.js
+++ code/grunt/gruntfile.js
@@ -4,48 +4,85 @@ const loadGruntTasks = require('load-grunt-tasks')
 
 module.exports = grunt => {
   grunt.initConfig({
-    uglify: {
-      options: {},
-      static_mappings: {
-        files: [{
-          src: 'node_modules/jquery/dist/jquery.js',
-          dest: 'temp/jquery.js'
-        }, {
-          src: 'node_modules/popper.js/dist/umd/popper.js',
-          dest: 'temp/popper.js'
-        }, {
-          src: 'node_modules/bootstrap/dist/js/bootstrap.js',
-          dest: 'temp/bootstrap.js'
-        }]
-      }
-    },
-    concat: {
-      js: {
-        src: ['temp/*.js'],
-        dest: 'dist/assets/scripts/vendor.js',
-        options: {
-          separator: ';',
-          stripBanners: true
-        }
-      }
-    },
-    sass: {
+    // 模板引擎
+    swig: {
       options: {
-        sourceMap: true,
-        implementation: sass
+        data: {
+          pkg: {
+            name: 'grunt page',
+            homepage: 'https://github.com/cinyearchan/fed-e-task-02-01/tree/master/code/grunt',
+            author: {
+              name: 'cinyearchan',
+              url: 'https://github.com/cinyearchan'
+            },
+            description: 'this is a fed grunt task project'
+          },
+          date: '2020-06-03'
+        },
+        cache: false,
+        templatePath: undefined
       },
-      main: {
+      mappings: {
+        expand: true,
+        cwd: 'src',
+        ext: '.html',
+        src: ['*.html'],
+        dest: 'dist'
+      }
+    },
+
+    // 合并文件
+    concat: {
+      // 合并 JS
+      kityminderJs: {
+        src: [
+          'node_modules/jquery/dist/jquery.js',
+          'node_modules/popper.js/dist/umd/popper.js',
+          'node_modules/bootstrap/dist/js/bootstrap.js'
+        ],
+        dest: 'temp/vendor.js'
+      }
+    },
+    // 压缩 JS
+    uglify: {
+      baseJs: {
+        src: 'temp/vendor.js',
+        dest: 'dist/assets/scripts/vendor.js'
+      }
+    },
+    // sass 转 css
+    sass: {
+      // grunt-sass
+      // options: {
+      //   sourceMap: true,
+      //   implementation: sass
+      // },
+      // main: {
+      //   files: {
+      //     'dist/assets/styles/main.css': 'src/assets/styles/main.scss'
+      //   }
+      // }
+
+      // grunt-contrib-sass
+      dist: {
+        options: {
+          style: 'expanded',
+          sourceMap: true,
+          implementation: sass
+        },
         files: {
           'dist/assets/styles/main.css': 'src/assets/styles/main.scss'
         }
       }
     },
+    // 压缩 CSS
     cssmin: {
       kopBaseCss: {
         src: ['node_modules/bootstrap/dist/css/bootstrap.css'],
         dest: "dist/assets/styles/vendor.css"
       }
     },
+    // 转换 JS
     babel: {
       options: {
         sourceMap: true,
@@ -57,6 +94,7 @@ module.exports = grunt => {
         }
       }
     },
+    // 清理临时文件
     clean: {
       beforeBuild: {
         files: [
@@ -73,6 +111,7 @@ module.exports = grunt => {
         ]
       }
     },
+    // 拷贝静态资源
     copy: {
       fonts: {
         expand: true,
@@ -90,7 +129,27 @@ module.exports = grunt => {
       },
       ico: {
         src: 'public/favicon.ico',
-        dest: 'dist/'
+        dest: 'dist/favicon.ico'
+      }
+    },
+    // 替换静态资源链接前置准备
+    useminPrepare: {
+      html: 'dist/*.html',
+      options: {
+        dest: './dist',
+        root: './dist'
+      }
+    },
+    // 替换静态资源链接
+    usemin: {
+      html: {
+        files: [{
+          src: 'dist/*.html',
+
+        }]
+      },
+      options: {
+        assetsDirs: ['dist']
       }
     }
   })
@@ -100,12 +159,15 @@ module.exports = grunt => {
   grunt.registerTask('default', ['sass', 'babel', 'copy'])
   grunt.registerTask('build', [
     'clean:beforeBuild',
+    'swig',
     'sass',
     'copy',
-    'uglify',
     'concat',
+    'uglify',
     'cssmin',
     'babel',
+    'useminPrepare',
+    'usemin',
     'clean:afterBuild'
   ])
 }
